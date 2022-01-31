5760a6e49273f2b7085643e8006d32662526dfb9
O Moo <omoog10101@gmai.com>
2022-01-30 22:45:21 -0700

    created buttons to do stuff

--- homepage/src/App.js
+++ homepage/src/App.js
@@ -3,15 +3,22 @@ import './css/App.css';
 
 
 function App() {
+
   return (
     <div className="App">
       <div id="content-box">
-        <p>Content</p>
-        <p>Another things</p>
+        <p id="content1">Content</p>
+        <p id="content2">Another things</p>
 
 
       </div>
-      <div id="contact-box"> <p> contact</p> </div>
+      <div id="contact-box"> <p> contact </p>
+      <button id="button">User 1</button>
+      <br></br>
+      <hr></hr>
+      <button id="button">User 2</button>
+
+      </div>
 
 
     </div>
