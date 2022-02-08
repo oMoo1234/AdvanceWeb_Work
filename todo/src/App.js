707cc11f9a831b8471ba356df03bf2a4a9ebe277
O Moo <omoog10101@gmai.com>
2022-02-08 16:26:50 -0700

    created server

--- todo/src/App.js
+++ todo/src/App.js
@@ -2,20 +2,15 @@ import React,{useState} from 'react';
 import './App.css';
 import { v4 as uuidv4 } from 'uuid';
 function App() {
-
   const initialList =[];
   const [list, setList] = useState(initialList);
   const [text, setName] = useState('');
-
   function handleChange(event) {
     setName(event.target.value);
   }
 
   function handleAdd() {
-    if(text === ''){
-
-
-    }
+    if(text === ''){}
     else{
     const newList = list.concat({ text, id: uuidv4() });
     setList(newList);
@@ -27,28 +22,22 @@ function App() {
     const newList = list.filter((item) => item.id !== id);
     setList(newList);
   }
-
   return (
     <div className="App">
       <header className="App-header">
        <h1>Add Things To Do!</h1>
       </header>
       <div className='body'>
-
         <input type="text" className='input' value={text} placeholder='Type here' onChange={handleChange} ></input>
-
         <button type="button" className='button' onClick={handleAdd}>
           Add
         </button>
-
       <ul>
         {list.map((item) => (
           <li key={item.id}>{item.text}
           <button className='button2' onClick={() => deleteItem(item.id)}>Done</button>
           </li>
-        ))
-
-        }
+        ))}
       </ul>
       </div>
     </div>
