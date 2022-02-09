655e542c3628101b343e9f5d6d3e3b691ea5c37b
O Moo <omoog10101@gmai.com>
2022-02-09 12:39:37 -0700

    created myapp

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+var user = [
+  {"id": 2,
+  "name": "O Moo"
+}]
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
